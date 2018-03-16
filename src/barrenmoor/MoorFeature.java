9af375ce6b698c94dba11466c4fe777e5218688f
Chad Stainbank <chad.stainbank@gmail.com>
2018-03-16 11:45:23 +0000

    Disambiguate 'feature location' from 'distance from player'

--- src/barrenmoor/MoorFeature.java
+++ src/barrenmoor/MoorFeature.java
@@ -4,8 +4,9 @@ public abstract class MoorFeature {
 	/*Easting and Northing are traditional cartographic terms meaning
 	 * _distance East_ and _distance North_ from Cartesian map origin (0,0).
 	 * Here they represent the distances East and North (in arbitrary units)
-	 * of a feature from the player.
-	 * In effect, the player is fixed and the world moves around them.
+	 * of a feature from the player. This tuple is referred to as the feature's
+	 * "location", which changes as the player moves. In effect, the player
+	 * is fixed and the world moves around them.
 	 */
 	int easting;
 	int northing;
@@ -13,5 +14,6 @@ public abstract class MoorFeature {
 	public double getDistanceFromPlayer() {
 		return Math.sqrt(Math.pow(easting, 2) + Math.pow(northing, 2));
 	}
+
 
 }
