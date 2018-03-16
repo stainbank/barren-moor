7c5866ced1196028633efae814924c015c8cbe9a
Chad Stainbank <chad.stainbank@gmail.com>
2018-03-16 11:41:51 +0000

    Enforce E, N order of Easting and Northing

--- src/barrenmoor/MoorFeature.java
+++ src/barrenmoor/MoorFeature.java
@@ -1,14 +1,17 @@
 package barrenmoor;
 
 public abstract class MoorFeature {
-	/*Northing and Easting are traditional cartographic terms meaning
-	 * _distance North_ and _distance East_ from Cartesian map origin (0,0).
-	 * Here they represent distance North/East from player in arbitrary units.
+	/*Easting and Northing are traditional cartographic terms meaning
+	 * _distance East_ and _distance North_ from Cartesian map origin (0,0).
+	 * Here they represent the distances East and North (in arbitrary units)
+	 * of a feature from the player.
+	 * In effect, the player is fixed and the world moves around them.
 	 */
-	int northing;
 	int easting;
+	int northing;
 	
 	public double getDistanceFromPlayer() {
-		return Math.sqrt(Math.pow(northing, 2) + Math.pow(easting, 2));
+		return Math.sqrt(Math.pow(easting, 2) + Math.pow(northing, 2));
 	}
+
 }
