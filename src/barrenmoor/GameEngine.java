c9b97d7151c44903b3d7179d11b4088bee7b8518
Chad Stainbank <chad.stainbank@gmail.com>
2018-03-16 20:40:42 +0000

    Move direction/bearing logic to own class

--- src/barrenmoor/GameEngine.java
+++ src/barrenmoor/GameEngine.java
@@ -1,34 +1,17 @@
 package barrenmoor;
-import java.util.HashMap;
 
 class GameEngine {
 	DisplayManager display = new DisplayManager();
 	MoorFeature treasure;
-	static HashMap<String, Integer> directionToBearing = new HashMap<String, Integer>();
-	static HashMap<Integer, String> eastingToDirection = new HashMap<Integer, String>();
-	static HashMap<Integer, String> northingToDirection = new HashMap<Integer, String>();
 
 	GameEngine(){
 		initialiseGame();
-		buildDirectionMaps();
 	}
 
 	void initialiseGame() {
 		generateTreasure();
 	}
 	
-	void buildDirectionMaps() {
-		directionToBearing.put("E", 1);
-		directionToBearing.put("N", 1);
-		directionToBearing.put("W", -1);
-		directionToBearing.put("S", -1);
-		
-		eastingToDirection.put(1, "E");
-		northingToDirection.put(1, "N");
-		eastingToDirection.put(-1, "W");
-		northingToDirection.put(-1, "S");
-	}
-
 	void playGame() {
 		display.display("Starting game");
 		playScriptedGame();
