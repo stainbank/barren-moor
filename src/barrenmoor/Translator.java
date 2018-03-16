05bf03df869826e3555aeb289970c00df407b2d7
Chad Stainbank <chad.stainbank@gmail.com>
2018-03-16 22:17:47 +0000

    Initialise translation maps statically

--- src/barrenmoor/Translator.java
+++ src/barrenmoor/Translator.java
@@ -5,13 +5,12 @@ class Translator {
 	static HashMap<String, Integer> directionToBearingSign = new HashMap<String, Integer>();
 	static HashMap<Integer, String> eastingToDirection = new HashMap<Integer, String>();
 	static HashMap<Integer, String> northingToDirection = new HashMap<Integer, String>();
-
-	Translator(){
+	static {
 		directionToBearingSign.put("E", 1);
 		directionToBearingSign.put("N", 1);
 		directionToBearingSign.put("W", -1);
 		directionToBearingSign.put("S", -1);
-		
+
 		eastingToDirection.put(1, "E");
 		northingToDirection.put(1, "N");
 		eastingToDirection.put(-1, "W");
