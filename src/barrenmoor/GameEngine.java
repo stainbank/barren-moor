244d46faede07a8ae2572509ab096e23cf649bcd
Chad Stainbank <chad.stainbank@gmail.com>
2018-03-16 13:35:07 +0000

    Start displaying scripted game

--- src/barrenmoor/GameEngine.java
+++ src/barrenmoor/GameEngine.java
@@ -10,4 +10,12 @@ class GameEngine {
 	void generateTreasure() {
 		this.treasure = new Treasure(4, 6); // temporary default location
 	}
+	
+	void displayMessage(String messageToUser) {
+		System.out.println(messageToUser);
+	}
+	
+	void playGame() {
+		displayMessage("Starting game");
+	}
 }
